e22f1e749f8b4933c8755dd33ba274205f3092b1
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-06 10:51:12 -0600

    Added variables

---
+++ Awards Ceremony/main.py
@@ -0,0 +1,17 @@
+#Nicholas Larsen Awards ceremony project
+
+missingStaff = 0
+
+staff = 32 - missingStaff
+
+missingStudents = 0
+
+students = 100 % missingStudents
+
+missingGuests = 0
+
+guests = (students * 2) % missingGuests
+
+tableSize = 12
+
+print("You will need", (guests+students+staff)/12)
