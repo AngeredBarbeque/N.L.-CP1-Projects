f5c8bfb32206a014f4213c8b1aff7efe1dade58a
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-09 10:27:22 -0600

    Added resting variables.

--- Awards Ceremony/main.py
+++ Awards Ceremony/main.py
@@ -1,18 +1,26 @@
 #Nicholas Larsen Awards ceremony project
 
+missingStaff = 0
+
 missingStaff = 3
 
 staff = 32 - missingStaff
 
+missingStudents = 0
+
 missingStudents = 1
 
 students = 100 - missingStudents
+
+missingGuests = 0
 
 missingGuests = 15
 
 guests = (students * 2) - missingGuests
 
 tableSize = 12
+
+additionalGuests = 0
 
 additionalGuests = 1
 
