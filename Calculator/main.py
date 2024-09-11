10e36044e5d3c6c3c535ba0fc2c4c1522245d1fc
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-11 10:50:55 -0600

    Added basics

---
+++ Calculator/main.py
@@ -0,0 +1,5 @@
+#Nicholas Larsen Simple Caclulator
+
+choice = input("What operation do you want? type 1 for addition, type 2 for subraction, 3 for multiplication, 4 for division, 5 for modulo, 6 for exponents, or 7 for rounded division.")
+number = input("Which is the first of the numbers thee shalt be preforming thine operations upon?")
+numberTwo = input("Which is the second of the numbers thee shalt be preforming thine operations upon?")
