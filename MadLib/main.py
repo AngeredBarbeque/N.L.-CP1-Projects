1766eab541a8087422be63470fdfe822b80aed85
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-12 10:51:00 -0600

    Started Basics

---
+++ MadLib/main.py
@@ -0,0 +1,9 @@
+#Nick Larsen MadLib
+
+noun = input("What is your noun?")
+verb = input("What is your verb?")
+sound = input("What is an adjective to describe a sound?")
+adjective = input("What is an adjective?")
+name = input("What is a name?")
+
+print("Once upon a time, there was young", noun, "named", name, ".", name, "loved to", verb, "and did it all day. But", name, "'s parents were", adjective, "and hated when", name, "would", verb, ". It always made a", sound, "sound. So they came up with a plot. They would overthrow the queen and make all", verb, "ing illegal! But", name, "had other plans. they ran away to the kingdom of ducks, and stole a barrel of fruit. They lived in that barrel for the next hundred years, and were buried in that barrel.")
