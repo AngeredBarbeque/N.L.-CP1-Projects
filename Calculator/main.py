2e6af2fa292479788e5aff7819ca34e962aef1dc
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-12 10:25:26 -0600

    Bullying

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,5 +1,25 @@
 #Nicholas Larsen Simple Caclulator
 
-choice = input("What operation do you want? type 1 for addition, type 2 for subraction, 3 for multiplication, 4 for division, 5 for modulo, 6 for exponents, or 7 for rounded division.")
-number = input("Which is the first of the numbers thee shalt be preforming thine operations upon?")
-numberTwo = input("Which is the second of the numbers thee shalt be preforming thine operations upon?")
+choice = int(input("What operation do you want? Type 1 for addition, 2 for subraction, 3 for multiplication, 4 for division, 5 for modulo, 6 for exponents, or 7 for rounded division."))
+number = int(input("Which is the first of the numbers thee shalt be preforming thine operations upon?"))
+numberTwo = int(input("Which is the second of the numbers thee shalt be preforming thine operations upon?"))
+
+if choice == 1:
+    answer = number + numberTwo
+elif choice == 2:
+    answer = number - numberTwo
+elif choice == 3:
+    answer = number * numberTwo
+elif choice == 4:
+    answer = number / numberTwo
+elif choice == 5:
+    answer = number % numberTwo
+elif choice == 6:
+    answer = number ** numberTwo
+elif choice == 7:
+    answer = number // numberTwo
+else: 
+    answer = "That's not a number, you buffalo!"
+
+print("The answer is", answer, "why couldn't you do that yourself?")
+
