e6af4de70c20c74612cc09760c8b8f17d1bb608b
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-12 10:35:07 -0600

    Added Palindrome checker

---
+++ Palindrome Checker/main.py
@@ -0,0 +1,9 @@
+#Nicholas Larsen Palindrome Checker
+
+word = input("What word would you like to check?")
+
+if word [::-1] == word: 
+    print("Yes, this is a palindrome. Loser.")
+else: print("No, this is not a palindrome. Idiot.")
+
+    
