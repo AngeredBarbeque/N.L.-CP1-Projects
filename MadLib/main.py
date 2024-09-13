839a60ed0114a229dda34a4607ec2e97c313cf3d
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-13 10:30:29 -0600

    MadLive

--- MadLib/main.py
+++ MadLib/main.py
@@ -1,9 +1,9 @@
 #Nick Larsen MadLib
 
-noun = input("What is your noun?")
-verb = input("What is your verb?")
-sound = input("What is an adjective to describe a sound?")
-adjective = input("What is an adjective?")
-name = input("What is a name?")
+noun = input("What is your noun?: ")
+verb = input("What is your verb?: ")
+adjectiveA = input("What is an adjective?: ")
+adjectiveB = input("What is an adjective?: ")
+name = input("What is a name?: ")
 
-print("Once upon a time, there was young", noun, "named", name, ".", name, "loved to", verb, "and did it all day. But", name, "'s parents were", adjective, "and hated when", name, "would", verb, ". It always made a", sound, "sound. So they came up with a plot. They would overthrow the queen and make all", verb, "ing illegal! But", name, "had other plans. they ran away to the kingdom of ducks, and stole a barrel of fruit. They lived in that barrel for the next hundred years, and were buried in that barrel.")
+print("Once upon a time, there was a", noun, "named "+name+".", name, "loved to "+verb+".", name, "was", adjectiveA, "and", adjectiveB, "but they didn't mind. When they were 22 years old, they moved to the land of the ducks. They then lived in a barrel for the rest of their lives. They also continued to", verb, "for the rest of their lives.")
