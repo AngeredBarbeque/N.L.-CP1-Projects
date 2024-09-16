ca4714895fc6752f04582f41676f29bf481827da
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-09-16 12:25:52 -0600

    Maybe fixed it

--- Fibbonaci/main.py
+++ Fibbonaci/main.py
@@ -2,6 +2,7 @@
 
 numOne = 0
 numTwo = 1
+iteratiions = 0
 
 print(numOne, numTwo)
 def findNextNumber(numOne, numTwo):
@@ -9,7 +10,8 @@ def findNextNumber(numOne, numTwo):
     print(numThree)
     numOne = numTwo
     numTwo = numThree
+    iterations += 1
 
-findNextNumber
+while iteratiions < 10: findNextNumber
 
 
