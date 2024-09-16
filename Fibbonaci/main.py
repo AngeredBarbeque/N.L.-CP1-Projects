3219c48ef5aedaa4d4ed88776d16e24afd080b33
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-16 10:50:59 -0600

    AHHHHH

---
+++ Fibbonaci/main.py
@@ -0,0 +1,15 @@
+#Nicholas Larsen Fibbonaci
+
+numOne = 0
+numTwo = 1
+
+print(numOne, numTwo)
+def findNextNumber(numOne, numTwo):
+    numThree = numOne + numTwo
+    print(numThree)
+    numOne = numTwo
+    numTwo = numThree
+
+findNextNumber
+
+
