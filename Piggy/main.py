1bb15ba91413fed02188eecfab5dab7e6d285bfd
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-23 10:44:33 -0600

    Ig Pay Atin Lay

---
+++ Piggy/main.py
@@ -0,0 +1,10 @@
+#Nicholas Larsen Pig Latin converter.
+
+def piggyMode(word):
+    firstLet = word[0]+"ay"
+    length = len(word)
+    N = length-1
+    word = word[length-N:]
+    return(word, firstLet,)
+
+print(piggyMode(word=input("What word would you like to convert?")))
