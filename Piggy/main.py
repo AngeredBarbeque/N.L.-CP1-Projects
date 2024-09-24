cca5b38ccf15c556417f2de374710b5e2ad44080
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-09-24 10:36:15 -0600

    Simplified

--- Piggy/main.py
+++ Piggy/main.py
@@ -1,10 +1,8 @@
 #Nicholas Larsen Pig Latin converter.
 
 def piggyMode(word):
-    firstLet = word[0]+"ay"
-    length = len(word)
-    N = length-1
-    word = word[length-N:]
+    firstLet = word[0]+ "ay"
+    word = word[1:]
     return(word + firstLet).title()
 
 print(piggyMode(word=input("What word would you like to convert?")))
