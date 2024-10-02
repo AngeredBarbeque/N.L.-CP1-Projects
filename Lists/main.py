46182609a8a444598122773a5cd523effc6929c3
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-10-02 10:00:14 -0600

    Print each seperately

---
+++ Lists/main.py
@@ -0,0 +1,9 @@
+classes = ["Geography", "CS 1400", "Secondary Math", "Lunch", "Advisory", "CSP", "Chemistry", "English 9H"]
+print(classes[0])
+print(classes[1])
+print(classes[2])
+print(classes[3])
+print(classes[4])
+print(classes[5])
+print(classes[6])
+print(classes[7])
