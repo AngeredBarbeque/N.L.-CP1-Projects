502e3df512fc5918701213f25481593cff15bc65
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-10-03 10:34:51 -0600

    Fixed

--- What are these/main.py
+++ What are these/main.py
@@ -1,7 +1,7 @@
 #Nicholas Larsen
 
 number = float(input("Please input a number: "))
-print("Your number is {:,}".format(int(number)))
-print("or {:.4f}".format(number))
-print("or {:.0%}".format(number))
-print("or ${:.2f}".format(number))
+print("Your number with commas to seperate thousands is {:,}.".format(int(number)))
+print("Your money with 4 decimal points is {:.4f}.".format(number))
+print("Your money as a percentage is {:.0%}.".format(number))
+print("Your number as money is ${:.2f}.".format(number))
