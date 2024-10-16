b45789741bf1601bb310d13025065d645319b4bf
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-10-16 10:24:37 -0600

    For loop

---
+++ Count up and down/main.py
@@ -0,0 +1,11 @@
+#Nicholas Larsen
+
+for i in range (21):
+        print(i)
+        i += 1
+else:
+        i = 20
+        for item in range (20):
+            i= i-1
+            print(i)
+        
