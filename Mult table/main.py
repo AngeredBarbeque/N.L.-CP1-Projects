19b871e87aee090c1b1822f497a87775afe54bdd
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-10-16 10:34:32 -0600

    tablemode

--- Mult table/main.py
+++ Mult table/main.py
@@ -1,4 +1,5 @@
-num = int(input("What number would you like to see multiples of?: "))
+#num = int(input("What number would you like to see multiples of?: "))
 
 for i in range(1, 13):
-    print(num*i)
+    print(1*i, "|", 2*i, "|", 3*i, "|", 4*i, "|", 5*i, "|", 6*i, "|", 7*i, "|", 8*i, "|", 9*i, "|", 10*i, "|", 11*i, "|", 12*i, )
+    
