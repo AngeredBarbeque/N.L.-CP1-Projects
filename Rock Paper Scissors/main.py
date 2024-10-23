b5feba2d8be84d0038225e78f4143eb3ae420016
angeredbarbeque <nick.larsen@ucas-edu.net>
2024-10-23 10:50:12 -0600

    socre

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -1,36 +1,44 @@
 #Nicholas Larsen rock paper scissors
 import random
 score = 0
-CPUscore = 0
+cpuscore = 0
 playing = "Y"
 print("Welcome to rock paper scissors!")
 while playing == "Y":
     userChoice = input("Rock, Paper, or Scissors?")
-    CPUChoice = random.randint(1, 3)
-    if CPUChoice == 1:
-        CPUChoice = "rock"
-    if CPUChoice == 2:
-        CPUChoice = "paper"
-    if CPUChoice == 3:
-        CPUChoice = "scissors"
-    print(CPUChoice)
-    if CPUChoice == userChoice:
+    cpuChoice = random.randint(1, 3)
+    if cpuChoice == 1:
+        cpuChoice = "rock"
+    if cpuChoice == 2:
+        cpuChoice = "paper"
+    if cpuChoice == 3:
+        cpuChoice = "scissors"
+    print(cpuChoice)
+    if cpuChoice == userChoice:
         print("You tied!")
-    elif CPUChoice == "rock" and userChoice == "paper":
+    elif cpuChoice == "rock" and userChoice == "paper":
         print("You won!")
-    elif CPUChoice == "rock" and userChoice == "scissors":
+        score += 1
+    elif cpuChoice == "rock" and userChoice == "scissors":
         print("You lost.")
-    elif CPUChoice == "paper" and userChoice == "rock":
+        cpuscore += 1
+    elif cpuChoice == "paper" and userChoice == "rock":
         print("You lost.")
-    elif CPUChoice == "paper" and userChoice == "scissors":
+        cpuscore += 1
+    elif cpuChoice == "paper" and userChoice == "scissors":
         print("You won!")
-    elif CPUChoice == "scissors" and userChoice == "rock":
+        score += 1
+    elif cpuChoice == "scissors" and userChoice == "rock":
         print("You won!")
-    elif CPUChoice == "scissors" and userChoice == "paper":
+        score += 1
+    elif cpuChoice == "scissors" and userChoice == "paper":
         print("You lost.")
+        cpuscore += 1
     else:
         print("Sorry, please renter an input in all lowercase.")
         continue
+    print("Your score is", score)
+    print("CPU score is", cpuscore)
     playing = input("Play again? Y/N:")
 
 
