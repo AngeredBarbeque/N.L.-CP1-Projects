95c5d48817525f577b54f9d70da3523797764010
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-12-04 10:08:52 -0700

    Glad I lost points

--- Mult table/main.py
+++ Mult table/main.py
@@ -1,5 +1,11 @@
 for i in range(1, 13):
     for item in range(1, 13):
-        print(i*item, "|", end='')
+        if len(str(i*item)) == 2:
+            spaces = ' '
+        elif len(str(i*item)) == 1:
+            spaces = '  '
+        else:
+            spaces = ''
+        print(i*item, spaces + "|", end='')
     print('')
     
